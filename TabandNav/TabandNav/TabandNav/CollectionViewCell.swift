042149ded17eba072463fdb61aad9c87b15aaf64
helenhall <68516050+helenhall@users.noreply.github.com>
2021-07-21 22:23:27 -0400

    Add files via upload

---
+++ TabandNav/TabandNav/TabandNav/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  TabandNav
+//
+//  Created by Helen Hall on 7/21/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellContentView: UIView!
+    @IBOutlet weak var imageCell: UIImageView!
+    
+}
---
+++ TabandNav/TabandNav/TabandNav/ViewControllerIdeas.swift
@@ -0,0 +1,41 @@
+//
+//  ViewControllerIdeas.swift
+//  TabandNav
+//
+//  Created by Helen Hall on 7/21/21.
+//
+
+import UIKit
+
+class ViewControllerIdeas: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+//
+//    @IBAction func didTapButton1(_ sender: UIButton) {
+//
+//        let vc = UIViewController()
+//        vc.view.backgroundColor = .red
+//
+//        navigationController?.pushViewController(vc, animated: true)
+//    }
+//    
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
